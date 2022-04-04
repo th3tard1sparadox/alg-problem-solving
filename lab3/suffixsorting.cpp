b19ccbe8f3e0c97844019eb42c73b73a889d69d4
Annie Wång <annwa858@student.liu.se>
2022-04-04 16:14:46 +0200

    maybe windows stuff

--- lab3/suffixsorting.cpp
+++ lab3/suffixsorting.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -13,27 +14,51 @@ int compSuff(struct suffix first, struct suffix second) {
     return first.suff.compare(second.suff) < 0 ? 1 : 0;
 }
 
-vector<int> buildArray(string text, int n) {
-    struct suffix
+vector<int> buildArray(string & text, int n) {
+    struct suffix suffixes[n];
+
+    for(int i = 0; i < n; i++) {
+        suffixes[i].index = i;
+        suffixes[i].suff = text.substr(i);
+    }
+
+    sort(suffixes, suffixes+n, compSuff);
+
+    vector<int> suffArr(n);
+    for(int i = 0; i < n; i++) {
+        suffArr[i] = suffixes[i].index;
+    }
+
+    return suffArr;
 }
 
 int main() {
     // C++ i/o speedup.
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    // ios::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // cout.tie(NULL);
 
     string s;
     getline(cin, s);
+    cout << "string is: " << s << endl;
+    int sLen = s.length();
     int n;
     while(cin) {
+        cout << "string is: " << s << endl;
+        vector<int> suffArr = buildArray(s, sLen);
+        cout << "string is: " << s << endl;
         cin >> n;
+        cout << "n is: " << n << endl;
+        cout << "string is: " << s << endl;
         int query;
         for(int i = 0; i < n; i++) {
             cin >> query;
+            cout << suffArr[query] << " ";
         }
+        cout << endl;
 
         getline(cin, s);
+        sLen = s.length();
     }
 
     return 0;
