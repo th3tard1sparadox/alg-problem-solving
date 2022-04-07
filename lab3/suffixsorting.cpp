2a6ee38f42d7801a88f170ca85f7660d8ecf2519
Annie Wång <annwa858@student.liu.se>
2022-04-07 22:09:27 +0200

    comment code

--- lab3/suffixsorting.cpp
+++ lab3/suffixsorting.cpp
@@ -1,5 +1,17 @@
 /**
- * TODO: comment.
+ * @file suffixsorting.cpp
+ * @author Annie Wång (annwa858@student.liu.se)
+ * @brief an implementation of a suffixArray construction and index retreival.
+ *        The implementation is based on this paper: https://web.stanford.edu/class/cs97si/suffix-array.pdf
+ *        where the timecomplexity is more thoroughly explained. The implementation
+ *        is in short that we sort according to the first character then 
+ *        the 2 first, then the 4 first etc.. This is done by using the
+ *        "value" of the character as a rank to sort according to. The timecomplexity
+ *        for the construction of the array is O(nlognlogn) where nlogn 
+ *        comes from the standard sorting and logn from the implementation 
+ *        of the algorithm. If a radixsort were to be used instead of the standard 
+ *        sort (quicksort) a complexity of O(nlogn) would be reached.
+ * @date 2022-04-07
  */
 
 #include <iostream>
@@ -10,108 +22,84 @@
 
 using namespace std;
 
-enum Type {naive, mem, full};
-
-struct suffixF {
+// each suffix care about its starting index and its current and next rank.
+struct suffix {
     int index;
     int rank[2];
 };
 
-struct suffixN {
-    int index;
-    char* suff;
+// class implementing the suffix array.
+class suffixArray {
+    vector<int> array;
+
+private:
+    // how we compare two suffixes, first based on current rank then next rank.
+    static int suffixCmp(struct suffix first, struct suffix second) {
+        if(first.rank[0] == second.rank[0]) {
+            return (first.rank[1] < second.rank[1] ? 1 : 0);
+        } else {
+            return (first.rank[0] < second.rank[0] ? 1 : 0);
+        }
+    }
+
+public:
+    // constructor implementing the construction of a suffix array.
+    suffixArray(string s) {
+        int n = s.length();
+
+        // set up char* instead of string to save memory.        
+        char* text = new char[n + 1];
+        strcpy(text, s.c_str());
+
+        struct suffix suffixes[n];
+
+        for(int i = 0; i < n; i++) {
+            suffixes[i].index = i;
+            suffixes[i].rank[0] = text[i];
+            suffixes[i].rank[1] = ((i + 1) < n) ? (text[i + 1]) : -1;
+        }
+
+        sort(suffixes, suffixes+n, suffixCmp);
+
+        int indexes[n];
+
+        for(int i = 4; i < 2 * n; i = i * 2) {
+            int rank = 0;
+            int prevRank = suffixes[0].rank[0];
+            suffixes[0].rank[0] = rank;
+            indexes[suffixes[0].index] = 0;
+
+            for(int j = 1; j < n; j++) {
+                if(suffixes[j].rank[0] == prevRank && suffixes[j].rank[1] == suffixes[j - 1].rank[1]) {
+                    prevRank = suffixes[j].rank[0];
+                    suffixes[j].rank[0] = rank;
+                } else {
+                    prevRank = suffixes[j].rank[0];
+                    suffixes[j].rank[0] = ++rank;
+                }
+                indexes[suffixes[j].index] = j;
+            }
+
+            for(int j = 0; j < n; j++) {
+                int nextIndex = suffixes[j].index + i / 2;
+                suffixes[j].rank[1] = (nextIndex < n) ? suffixes[indexes[nextIndex]].rank[0] : -1;
+            }
+            sort(suffixes, suffixes+n, suffixCmp);
+        }
+
+        for(int i = 0; i < n; i++) {
+            array.push_back(suffixes[i].index);
+        }
+
+        // memory management.
+        delete[] text;
+    }
+
+    int getSuffix(int i) {
+        return array[i];
+    }
 };
 
-int compSuffF(struct suffixF first, struct suffixF second) {
-    if(first.rank[0] == second.rank[0]) {
-        return (first.rank[1] < second.rank[1] ? 1 : 0);
-    } else {
-        return (first.rank[0] < second.rank[0] ? 1 : 0);
-    }
-}
-
-int compSuffN(struct suffixN first, struct suffixN second) {
-    return strcmp(first.suff, second.suff) < 0 ? 1 : 0;
-}
-
-vector<int> buildArrayFull(char* text, int n) {
-    struct suffixF suffixes[n];
-
-    for(int i = 0; i < n; i++) {
-        suffixes[i].index = i;
-        suffixes[i].rank[0] = text[i];
-        suffixes[i].rank[1] = ((i + 1) < n) ? (text[i + 1]) : -1;
-    }
-
-    sort(suffixes, suffixes+n, compSuffF);
-
-    int indexes[n];
-
-    for(int i = 4; i < 2 * n; i = i * 2) {
-        int rank = 0;
-        int prevRank = suffixes[0].rank[0];
-        suffixes[0].rank[0] = rank;
-        indexes[suffixes[0].index] = 0;
-
-        for(int j = 1; j < n; j++) {
-            if(suffixes[j].rank[0] == prevRank && suffixes[j].rank[1] == suffixes[j - 1].rank[1]) {
-                prevRank = suffixes[j].rank[0];
-                suffixes[j].rank[0] = rank;
-            } else {
-                prevRank = suffixes[j].rank[0];
-                suffixes[j].rank[0] = ++rank;
-            }
-            indexes[suffixes[j].index] = j;
-        }
-
-        for(int j = 0; j < n; j++) {
-            int nextIndex = suffixes[j].index + i / 2;
-            suffixes[j].rank[1] = (nextIndex < n) ? suffixes[indexes[nextIndex]].rank[0] : -1;
-        }
-        sort(suffixes, suffixes+n, compSuffF);
-    }
-
-    vector<int> suffArr;
-    for(int i = 0; i < n; i++) {
-        suffArr.push_back(suffixes[i].index);
-    }
-
-    return suffArr;
-}
-
-vector<int> buildArrayNaive(char* text, int n) {
-    struct suffixN suffixes[n];
-
-    for(int i = 0; i < n; i++) {
-        suffixes[i].index = i;
-        suffixes[i].suff = (text+i);
-    }
-
-    sort(suffixes, suffixes+n, compSuffN);
-
-    vector<int> suffArr;
-    for(int i = 0; i < n; i++) {
-        suffArr.push_back(suffixes[i].index);
-    }
-
-    return suffArr;
-}
-
-vector<int> buildArrayMap(string & s, int sLen) {
-    map<string, int> m;
-    vector<int> suffArr;
-    
-    string subS = "";
-    for(int i =  sLen - 1; i >= 0; i--) {
-        subS = s[i] + subS;
-        m[subS] = i;
-    }
-
-    for(auto suffPos : m) {
-        suffArr.push_back(suffPos.second);
-    }
-    return suffArr;
-}
 
 int main() {
     // C++ i/o speedup.
@@ -119,45 +107,18 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    Type implementation = full;
+    // read input.
     string s;
     int n;
     while(getline(cin, s)) {
-        int sLen = s.length();
-        if(implementation == naive) {
-            char* newS = new char[sLen + 1];
-            strcpy(newS, s.c_str());
-            vector<int> suffArr = buildArrayNaive(newS, sLen);
-            cin >> n;
-            int query;
-            for(int i = 0; i < n; i++) {
-                cin >> query;
-                cout << suffArr[query] << " ";
-            }
-            cout << endl;
-            delete[] newS;
-        } else if(implementation == mem) {
-            vector<int> suffArr = buildArrayMap(s, sLen);
-            cin >> n;
-            int query;
-            for(int i = 0; i < n; i++) {
-                cin >> query;
-                cout << suffArr[query] << " ";
-            }
-            cout << endl;
-        } else if(implementation == full) {
-            char* newS = new char[sLen + 1];
-            strcpy(newS, s.c_str());
-            vector<int> suffArr = buildArrayFull(newS, sLen);
-            cin >> n;
-            int query;
-            for(int i = 0; i < n; i++) {
-                cin >> query;
-                cout << suffArr[query] << " ";
-            }
-            cout << endl;
-            delete[] newS;
+        suffixArray suffArr(s);
+        cin >> n;
+        int query;
+        for(int i = 0; i < n; i++) {
+            cin >> query;
+            cout << suffArr.getSuffix(query) << " ";
         }
+        cout << endl;
         getline(cin, s);
     }
 
