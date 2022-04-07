03ee3b9019503d0bf4c9a8d634018606d0f3847e
Annie Wång <annwa858@student.liu.se>
2022-04-07 14:46:44 +0200

    we did it boys

--- lab3/suffixsorting.cpp
+++ lab3/suffixsorting.cpp
@@ -1,6 +1,5 @@
 /**
- * This is not a final version as this is too slow and naive.
- * TODO: make fast.
+ * TODO: comment.
  */
 
 #include <iostream>
@@ -11,59 +10,154 @@
 
 using namespace std;
 
-// class Suffix {
-    // int index;
-    // string suff;
-// }
+enum Type {naive, mem, full};
 
-struct suffix {
+struct suffixF {
+    int index;
+    int rank[2];
+};
+
+struct suffixN {
     int index;
     char* suff;
 };
 
-int compSuff(struct suffix first, struct suffix second) {
+int compSuffF(struct suffixF first, struct suffixF second) {
+    if(first.rank[0] == second.rank[0]) {
+        return (first.rank[1] < second.rank[1] ? 1 : 0);
+    } else {
+        return (first.rank[0] < second.rank[0] ? 1 : 0);
+    }
+}
+
+int compSuffN(struct suffixN first, struct suffixN second) {
     return strcmp(first.suff, second.suff) < 0 ? 1 : 0;
 }
 
-vector<int> buildArray(char* text, int n) {
-    struct suffix suffixes[n];
+vector<int> buildArrayFull(char* text, int n) {
+    struct suffixF suffixes[n];
+
+    for(int i = 0; i < n; i++) {
+        suffixes[i].index = i;
+        suffixes[i].rank[0] = text[i];
+        suffixes[i].rank[1] = ((i + 1) < n) ? (text[i + 1]) : -1;
+    }
+
+    sort(suffixes, suffixes+n, compSuffF);
+
+    int indexes[n];
+
+    for(int i = 4; i < 2 * n; i = i * 2) {
+        int rank = 0;
+        int prevRank = suffixes[0].rank[0];
+        suffixes[0].rank[0] = rank;
+        indexes[suffixes[0].index] = 0;
+
+        for(int j = 1; j < n; j++) {
+            if(suffixes[j].rank[0] == prevRank && suffixes[j].rank[1] == suffixes[j - 1].rank[1]) {
+                prevRank = suffixes[j].rank[0];
+                suffixes[j].rank[0] = rank;
+            } else {
+                prevRank = suffixes[j].rank[0];
+                suffixes[j].rank[0] = ++rank;
+            }
+            indexes[suffixes[j].index] = j;
+        }
+
+        for(int j = 0; j < n; j++) {
+            int nextIndex = suffixes[j].index + i / 2;
+            suffixes[j].rank[1] = (nextIndex < n) ? suffixes[indexes[nextIndex]].rank[0] : -1;
+        }
+        sort(suffixes, suffixes+n, compSuffF);
+    }
+
+    vector<int> suffArr;
+    for(int i = 0; i < n; i++) {
+        suffArr.push_back(suffixes[i].index);
+    }
+
+    return suffArr;
+}
+
+vector<int> buildArrayNaive(char* text, int n) {
+    struct suffixN suffixes[n];
 
     for(int i = 0; i < n; i++) {
         suffixes[i].index = i;
         suffixes[i].suff = (text+i);
     }
 
-    sort(suffixes, suffixes+n, compSuff);
+    sort(suffixes, suffixes+n, compSuffN);
 
-    vector<int> suffArr(n);
+    vector<int> suffArr;
     for(int i = 0; i < n; i++) {
-        suffArr[i] = suffixes[i].index;
+        suffArr.push_back(suffixes[i].index);
     }
 
     return suffArr;
 }
 
+vector<int> buildArrayMap(string & s, int sLen) {
+    map<string, int> m;
+    vector<int> suffArr;
+    
+    string subS = "";
+    for(int i =  sLen - 1; i >= 0; i--) {
+        subS = s[i] + subS;
+        m[subS] = i;
+    }
+
+    for(auto suffPos : m) {
+        suffArr.push_back(suffPos.second);
+    }
+    return suffArr;
+}
+
 int main() {
     // C++ i/o speedup.
-    // ios::sync_with_stdio(false);
-    // cin.tie(NULL);
-    // cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
+    Type implementation = full;
     string s;
     int n;
     while(getline(cin, s)) {
         int sLen = s.length();
-        char* newS = new char[sLen + 1];
-        strcpy(newS, s.c_str());
-        vector<int> suffArr = buildArray(newS, sLen);
-        cin >> n;
-        int query;
-        for(int i = 0; i < n; i++) {
-            cin >> query;
-            cout << suffArr[query] << " ";
+        if(implementation == naive) {
+            char* newS = new char[sLen + 1];
+            strcpy(newS, s.c_str());
+            vector<int> suffArr = buildArrayNaive(newS, sLen);
+            cin >> n;
+            int query;
+            for(int i = 0; i < n; i++) {
+                cin >> query;
+                cout << suffArr[query] << " ";
+            }
+            cout << endl;
+            delete[] newS;
+        } else if(implementation == mem) {
+            vector<int> suffArr = buildArrayMap(s, sLen);
+            cin >> n;
+            int query;
+            for(int i = 0; i < n; i++) {
+                cin >> query;
+                cout << suffArr[query] << " ";
+            }
+            cout << endl;
+        } else if(implementation == full) {
+            char* newS = new char[sLen + 1];
+            strcpy(newS, s.c_str());
+            vector<int> suffArr = buildArrayFull(newS, sLen);
+            cin >> n;
+            int query;
+            for(int i = 0; i < n; i++) {
+                cin >> query;
+                cout << suffArr[query] << " ";
+            }
+            cout << endl;
+            delete[] newS;
         }
-        cout << endl;
-        delete[] newS;
         getline(cin, s);
     }
 
